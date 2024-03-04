8df9ce5e0a9cfcd7e677cd9c8c0b24df1edd371f
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-04 20:42:47 +0300

    feat: Добавил базовую логику в  класс "Map"

--- src/com/example/Map.java
+++ src/com/example/Map.java
@@ -1,5 +1,31 @@
 package com.example;
 
-public class Map {
+import com.example.entities.Entity;
 
+import java.util.HashMap;
+
+public class Map {
+    private HashMap<Coordinates, Entity> entities = new HashMap<>();
+
+    public void addEntity(Coordinates coordinates, Entity entity){
+        entities.put(coordinates, entity);
+    }
+
+    public void updateEntityPosition(Coordinates from, Coordinates to){
+        Entity entity = getEntity(from);
+        removeEntity(from);
+        addEntity(to, entity);
+    }
+
+    public void removeEntity(Coordinates coordinates){
+        entities.remove(coordinates);
+    }
+
+    public Entity getEntity(Coordinates coordinates){
+        return entities.get(coordinates);
+    }
+
+    public HashMap<Coordinates, Entity> getEntities() {
+        return entities;
+    }
 }
