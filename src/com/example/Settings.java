43df19e9f1e81d197bada4ebd6d09db45d172283
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-04 22:47:58 +0300

    feat: Реализовал метод "createMap" в классе "Settings"

--- src/com/example/Settings.java
+++ src/com/example/Settings.java
@@ -1,4 +1,15 @@
 package com.example;
 
 public class Settings {
+
+    public static Map createMap(int size){
+        switch (size){
+            case 1:
+                return new Map(8, 8 );
+            case 2:
+                return new Map(10, 10);
+            default:
+                return new Map(5, 5);
+        }
+    }
 }
