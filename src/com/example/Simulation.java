7fdd4e37fd142392d431751ffe951f909b0a5cf7
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-04 23:21:46 +0300

    feat: Реализов метод "renderMap" в классе "Simulation"

--- src/com/example/Simulation.java
+++ src/com/example/Simulation.java
@@ -1,4 +1,24 @@
 package com.example;
 
+import com.example.entities.Entity;
+
 public class Simulation {
+    public void renderMap(Map map){
+        for (int y = 0; y < map.getHeight(); y++){
+            for (int x = 0; x < map.getWidth(); x++){
+                boolean entityFound = false;
+                for (Entity entity: map.getEntities().values()) {
+                    if(entity.getCoordinates().getX() == x && entity.getCoordinates().getY() == y ){
+                        System.out.print(entity.getAppearance().charAt(0) + " ");
+                        entityFound = true;
+                        break;
+                    }
+                }
+                if(!entityFound){
+                    System.out.print(". ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
