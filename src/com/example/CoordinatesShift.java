735f7c8c7d08fc35b59b7790b94f36a8a36415a8
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-05 19:51:49 +0300

    feat: Добавил базовую логику в класс "CoordinatesShift"

--- src/com/example/CoordinatesShift.java
+++ src/com/example/CoordinatesShift.java
@@ -1,4 +1,42 @@
 package com.example;
 
+import java.util.Objects;
+
 public class CoordinatesShift {
+    private int x;
+    private int y;
+
+    public CoordinatesShift(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CoordinatesShift that = (CoordinatesShift) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
 }
