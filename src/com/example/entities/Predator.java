6f1430432f19d018197843838b17d964df831424
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-05 20:27:30 +0300

    feat: Реализовал метод "getPossibleMoves" в классе "Predator"

--- src/com/example/entities/Predator.java
+++ src/com/example/entities/Predator.java
@@ -3,6 +3,8 @@ package com.example.entities;
 import com.example.Coordinates;
 import com.example.CoordinatesShift;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Predator extends Creature{
@@ -15,6 +17,11 @@ public class Predator extends Creature{
 
     @Override
     protected Set<CoordinatesShift> getPossibleMoves() {
-        return null;
+        return new HashSet<>(Arrays.asList(
+                new CoordinatesShift(1, 0),
+                new CoordinatesShift(-1, 0),
+                new CoordinatesShift(0, -1),
+                new CoordinatesShift(0, 1)
+        ));
     }
 }
