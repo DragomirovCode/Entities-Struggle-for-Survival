295a3f8e8742afcbd307919af85d89af973cdb5c
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-06 15:49:06 +0300

    feat: Реализовал базовую логику метода "searchPath" в классе "Predator"

--- src/com/example/entities/Predator.java
+++ src/com/example/entities/Predator.java
@@ -4,9 +4,7 @@ import com.example.Coordinates;
 import com.example.CoordinatesShift;
 import com.example.Map;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Predator extends Creature{
     public Predator(String Appearance, Coordinates coordinates) {
@@ -27,7 +25,47 @@ public class Predator extends Creature{
     }
 
     @Override
-    protected Coordinates searchPath(Coordinates start, Coordinates end, Map map) {
-        return null;
+    public void searchPath(Coordinates start, Coordinates end, Map map) {
+        Queue<Coordinates> queue = new LinkedList<>();
+        HashMap<Coordinates, Coordinates> cameFrom = new HashMap<>();
+        Set<Coordinates> visited = new HashSet<>();
+
+        queue.add(start);
+        visited.add(start);
+        cameFrom.put(start, null);
+
+        while(!queue.isEmpty()){
+            Coordinates current = queue.poll();
+            System.out.println(current + " - текущяя вершина");
+
+            if(current.equals(end)) {
+                break;
+            }
+
+            for(CoordinatesShift shift : getPossibleMoves()) {
+                Coordinates next = current.shift(shift, map);
+                if((next != null && (!visited.contains(next)) || (Objects.requireNonNull(next).equals(end)) &&
+                        map.getAvailabilityStatusOfCoordinate(next))) {
+                    queue.add(next);
+                    visited.add(next);
+                    cameFrom.put(next, current);
+                }
+            }
+        }
+
+        if(!cameFrom.containsKey(end)) {
+            System.out.println("Путь не найден");
+            return;
+        }
+
+        Coordinates current = end;
+        List<Coordinates> path = new ArrayList<>();
+        while(current != null) {
+            path.add(current);
+            current = cameFrom.get(current);
+        }
+
+        Collections.reverse(path);
+        System.out.println("Найденный путь: " + path);
     }
 }
