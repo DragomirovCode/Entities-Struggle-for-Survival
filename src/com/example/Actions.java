204792827be9f920c7b5c93c1b1bb6b40f48600e
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-08 19:27:06 +0300

    chore: Переместил 2 метода в класс "Actions", "initActions", "greet"

--- src/com/example/Actions.java
+++ src/com/example/Actions.java
@@ -1,4 +1,31 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Actions {
+
+    private Simulation simulation = new Simulation();
+    private static String mapSize = "";
+    public void initActions(){
+        greet();
+        Mapping mapping = Settings.createMap(Integer.parseInt(mapSize));
+        mapping.FillRandomPositions(mapSize);
+        simulation.renderMap(mapping);
+    }
+
+    private void greet(){
+        System.out.println("Привет! Выбери размер карты");
+        System.out.println("1 - средней, 2 - большой, 3 - стандартный");
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            mapSize = scanner.nextLine();
+            switch (mapSize) {
+                case "1", "2", "3":
+                    return;
+                default:
+                    System.out.println("Пожалуйста, выберите один из предложенных вариантов");
+                    break;
+            }
+        }
+    }
 }
--- src/com/example/Simulation.java
+++ src/com/example/Simulation.java
@@ -2,10 +2,7 @@ package com.example;
 
 import com.example.entities.Entity;
 
-import java.util.Scanner;
-
 public class Simulation {
-    private static String mapSize = "";
     public void renderMap(Mapping map){
         for (int y = 0; y < map.getHeight(); y++){
             for (int x = 0; x < map.getWidth(); x++){
@@ -22,28 +19,6 @@ public class Simulation {
                 }
             }
             System.out.println();
-        }
-    }
-    public void initActions(){
-        greet();
-        Mapping mapping = Settings.createMap(Integer.parseInt(mapSize));
-        mapping.FillRandomPositions(mapSize);
-        renderMap(mapping);
-    }
-
-    private void greet(){
-        System.out.println("Привет! Выбери размер карты");
-        System.out.println("1 - средней, 2 - большой, 3 - стандартный");
-        Scanner scanner = new Scanner(System.in);
-        while(true){
-            mapSize = scanner.nextLine();
-            switch (mapSize) {
-                case "1", "2", "3":
-                    return;
-                default:
-                    System.out.println("Пожалуйста, выберите один из предложенных вариантов");
-                    break;
-            }
         }
     }
 }
