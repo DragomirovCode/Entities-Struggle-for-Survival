969604560bc6f2a2bdf5d7cb5c3db50404d15cc0
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-15 18:59:27 +0300

    feat: Добавил метод "requestSpeedFromUser"

--- src/com/example/Actions.java
+++ src/com/example/Actions.java
@@ -3,14 +3,20 @@ package com.example;
 import java.util.Scanner;
 
 public class Actions {
+    private Scanner scanner = new Scanner(System.in);;
     private Simulation simulation = new Simulation(this);
     private Mapping currentMap;
 
+    public static int speed;
+
     private static String mapSize = "";
+    public static String speedSize = "";
     public void initActions(){
         greet();
+        requestSpeedFromUser();
         currentMap = Settings.createMap(Integer.parseInt(mapSize));
         currentMap.fillRandomPositions(mapSize);
+        speed = Settings.calculateSpeed(Integer.parseInt(speedSize));
         setCurrentMap(currentMap);
         simulation.renderMap(currentMap);
     }
@@ -26,10 +32,23 @@ public class Actions {
     private void greet(){
         System.out.println("Привет! Выбери размер карты");
         System.out.println("1 - средней, 2 - большой, 3 - стандартный");
-        Scanner scanner = new Scanner(System.in);
         while(true){
             mapSize = scanner.nextLine();
             switch (mapSize) {
+                case "1", "2", "3":
+                    return;
+                default:
+                    System.out.println("Пожалуйста, выберите один из предложенных вариантов");
+                    break;
+            }
+        }
+    }
+    private void requestSpeedFromUser(){
+        System.out.println("Выбери скорость существам");
+        System.out.println("1 - стандартная, 2 - средняя, 3 - медлинная");
+        while(true){
+            speedSize = scanner.nextLine();
+            switch (speedSize) {
                 case "1", "2", "3":
                     return;
                 default:
