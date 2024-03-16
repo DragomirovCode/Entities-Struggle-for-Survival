3f6f2b4d4f91056393769be9c79698612ef3dca7
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-16 10:58:36 +0300

    feat: Унаследовал класс "AbstractAnimal"

--- src/com/example/entities/Herbivore.java
+++ src/com/example/entities/Herbivore.java
@@ -1,36 +1,10 @@
 package com.example.entities;
 
+import com.example.AbstractAnimal;
 import com.example.Coordinates;
-import com.example.CoordinatesShift;
-import com.example.Mapping;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-public class Herbivore extends Creature{
+public class Herbivore extends AbstractAnimal {
     public Herbivore(String Appearance, Coordinates coordinates) {
         super(Appearance, coordinates);
-    }
-
-    @Override
-    public void makeMove(Coordinates from, Coordinates to, List<Coordinates> path, Mapping map, Entity entity) {
-
-    }
-
-    @Override
-    protected Set<CoordinatesShift> getPossibleMoves() {
-        return new HashSet<>(Arrays.asList(
-                new CoordinatesShift(1, 0),
-                new CoordinatesShift(-1, 0),
-                new CoordinatesShift(0, -1),
-                new CoordinatesShift(0, 1)
-        ));
-    }
-
-    @Override
-    public List<Coordinates> searchPath(Coordinates start, Coordinates end, Mapping map) {
-        return null;
     }
 }
