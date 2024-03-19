786cca68e48fe8ebbadd6104f80495dfc7cdc95c
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-19 23:13:38 +0300

    feat: Добавил логику завершения симуляции

--- src/com/example/PauseSimulation.java
+++ src/com/example/PauseSimulation.java
@@ -1,8 +1,26 @@
 package com.example;
 
-public class PauseSimulation implements Runnable {
-    @Override
-    public void run() {
+import java.util.Scanner;
 
+public class PauseSimulation extends Thread {
+    Scanner scanner = new Scanner(System.in);
+    public static boolean running = true;
+    public void stopTread() {
+        running = false;
+    }
+
+    public void run() {
+        while (running) {
+            try {
+                String tread = scanner.nextLine();
+                if(tread.equals("0")){
+                    System.out.println("Симуляция завершена.");
+                    stopTread();
+                }
+                Thread.sleep(200); // Ждем некоторое время перед следующей итерацией
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
