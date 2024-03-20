b37585519b6f805d36494beb9f79f3d95429ac0e
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-20 20:50:28 +0300

    chore: Изменил масштаб карты, изменил ХП "polypithHP"

--- src/com/example/Settings.java
+++ src/com/example/Settings.java
@@ -6,17 +6,19 @@ public class Settings {
     public static String grassAppearance = "G";
     public static String rockAppearance = "R";
     public static String treeAppearance = "T";
+    public static String polypithAppearance = "?";
 
     public int PredatorHP = 2;
     public int HerbivoreHP = 2;
     public int GrassHP = 2;
     public int RockHP = 0;
     public int TreeHP = 0;
+    public int polypithHP = 2;
 
     public static Mapping createMap(int size){
         return switch (size) {
-            case 1 -> new Mapping(9, 9);
-            case 2 -> new Mapping(10, 10);
+            case 1 -> new Mapping(11, 11);
+            case 2 -> new Mapping(13, 13);
             default -> new Mapping(8, 8);
         };
     }
