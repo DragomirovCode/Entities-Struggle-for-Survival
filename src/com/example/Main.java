151e3579eb052dcb7e798c873908368c8b6a7d85
Mikhail Dragomirov <mikhail_dragomirov@mail.ru>
2024-03-22 20:42:26 +0300

    chore: добавил в класс "Main" исклюение

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,7 +1,9 @@
 package com.example;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         Actions actions = new Actions();
         actions.initActions();
     }
